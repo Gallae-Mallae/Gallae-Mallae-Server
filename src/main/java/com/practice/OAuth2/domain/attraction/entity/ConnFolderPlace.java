2e43b7ab4b4fb114a0f2697297518679571d53b4
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2025-12-02 18:35:01 +0900

    feature : attraction entity 나머지 구축

---
+++ src/main/java/com/practice/OAuth2/domain/attraction/entity/ConnFolderPlace.java
@@ -0,0 +1,43 @@
+package com.practice.OAuth2.domain.attraction.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "conn_folder_place", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"folder_id", "attr_id"})
+})
+@EntityListeners(AuditingEntityListener.class)
+public class ConnFolderPlace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "folder_id", nullable = false)
+    private PlaceFolder placeFolder;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "attr_id", nullable = false)
+    private Attraction attraction;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/practice/OAuth2/domain/attraction/entity/PlaceLike.java
@@ -0,0 +1,44 @@
+package com.practice.OAuth2.domain.attraction.entity;
+
+import com.practice.OAuth2.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "place_likes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"attr_id", "user_id"})
+})
+@EntityListeners(AuditingEntityListener.class)
+public class PlaceLike {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long likeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "attr_id", nullable = false)
+    private Attraction attraction;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
