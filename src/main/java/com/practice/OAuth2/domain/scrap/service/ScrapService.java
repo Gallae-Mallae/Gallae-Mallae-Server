0aeeb6323f1bc7e21e19deacd0844d9df45c3d83
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2025-12-04 22:05:41 +0900

    feature : Scrap도메인 scrapfolder 조회기능 추가 및 연동 dto 추가

--- src/main/java/com/practice/OAuth2/domain/scrap/service/ScrapService.java
+++ src/main/java/com/practice/OAuth2/domain/scrap/service/ScrapService.java
@@ -1,5 +1,5 @@
 package com.practice.OAuth2.domain.scrap.service;
-
+import com.practice.OAuth2.domain.scrap.dto.ScrapFolderResponse;
 import com.practice.OAuth2.domain.scrap.dto.ScrapReqest;
 import com.practice.OAuth2.domain.scrap.dto.ScrapReqest.CreateScrap;
 import com.practice.OAuth2.domain.scrap.dto.ScrapResponse;
@@ -8,6 +8,7 @@ import com.practice.OAuth2.domain.scrap.entity.ScrapFolder;
 import com.practice.OAuth2.domain.scrap.repository.ScrapFolderRepository;
 import com.practice.OAuth2.domain.scrap.repository.ScrapRepository;
 import com.practice.OAuth2.domain.user.entity.User;
+import com.practice.OAuth2.domain.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,13 +22,13 @@ public class ScrapService {
 
     private final ScrapFolderRepository scrapFolderRepository;
     private final ScrapRepository scrapRepository;
-    // userrepository 나중에 가져오기
+    private final UserRepository userRepository;
 
-    // 폴더 생성
+    // scrap folder 생성
     @Transactional
     public Long createScrapFolder(Long userId, ScrapReqest.CreateScrapFolder req){
-        //User user = userRepository.findById(userId)
-        //        .orElseThrow(() -> new IllegalArgumentException("유저 x"));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("유저 x"));
         ScrapFolder folder = ScrapFolder.builder()
                 //.user(user)
                 .name(req.getName())
@@ -36,7 +37,7 @@ public class ScrapService {
         return scrapFolderRepository.save(folder).getFolderId();
     }
 
-    // 스크랩 생성
+    // scrap 생성
     @Transactional
     public Long createScrap(Long userId, Long folderId, ScrapReqest.CreateScrap req){
         ScrapFolder folder = scrapFolderRepository.findById(folderId)
@@ -54,7 +55,18 @@ public class ScrapService {
         return scrapRepository.save(scrap).getScrapId();
     }
 
-    // 조회
+    // scrap folder 조회
+    public List<ScrapFolderResponse> getScrapFolders(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("유저 x"));
+
+        // 유저가 만든 폴더들 다 찾아오기
+        return scrapFolderRepository.findByUser(user).stream()
+                .map(ScrapFolderResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    // scrap 조회
     @Transactional
     public List<ScrapResponse> getScraps(Long folderId){
         ScrapFolder folder = scrapFolderRepository.findById(folderId)
