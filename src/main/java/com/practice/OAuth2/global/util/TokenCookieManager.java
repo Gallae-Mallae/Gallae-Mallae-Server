dab3d28528d6a546e787ca4e93048ab9add1a781
SHSong <99pyues2@gmail.com>
2025-12-08 00:10:31 +0900

    chore : lax해제 테스트 + 응답쿠키 path 설정

--- src/main/java/com/practice/OAuth2/global/util/TokenCookieManager.java
+++ src/main/java/com/practice/OAuth2/global/util/TokenCookieManager.java
@@ -9,7 +9,7 @@ public class TokenCookieManager {
 
     private static final String ACCESS_TOKEN_NAME = "access_token";
     private static final String REFRESH_TOKEN_NAME = "refresh_token";
-    private static final String REFRESH_TOKEN_PATH = "/api/auth/reissue";
+    private static final String REFRESH_TOKEN_PATH = "/api/auth";
 
     // 토큰 쿠키 굽기 (로그인, 재발급 용)
     public void addTokenCookies(HttpServletResponse response, String accessToken, String refreshToken, long accessAge, long refreshAge) {
@@ -19,7 +19,7 @@ public class TokenCookieManager {
                 .httpOnly(true)
                 .secure(true) // HTTPS 필수
                 .maxAge(accessAge)
-                .sameSite("Lax") // 프런트 배포 이전에 테스트용으로 lax 해제
+                .sameSite("None") // 프런트 배포 이전에 테스트용으로 lax 해제
                 .build();
 
         // Refresh Token
@@ -28,7 +28,7 @@ public class TokenCookieManager {
                 .httpOnly(true)
                 .secure(true)
                 .maxAge(refreshAge) // 브라우저 꺼도 유지됨 (Persistent Cookie)
-                .sameSite("Lax")
+                .sameSite("None")
                 .build();
 
         response.addHeader("Set-Cookie", accessCookie.toString());
@@ -42,7 +42,7 @@ public class TokenCookieManager {
                 .httpOnly(true)
                 .secure(true)
                 .maxAge(0)
-                .sameSite("Lax")
+                .sameSite("None")
                 .build();
 
         ResponseCookie refreshCookie = ResponseCookie.from(REFRESH_TOKEN_NAME, "")
@@ -50,7 +50,7 @@ public class TokenCookieManager {
                 .httpOnly(true)
                 .secure(true)
                 .maxAge(0)
-                .sameSite("Lax")
+                .sameSite("None")
                 .build();
 
         response.addHeader("Set-Cookie", accessCookie.toString());
