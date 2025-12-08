f98f46ae44eef02e165547ca740741f269b8a46d
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2025-12-08 20:20:08 +0900

    feature : 탈퇴 후 재로그인시 email(uk)로 복구 프로세스

--- src/main/java/com/practice/OAuth2/domain/user/repository/UserRepository.java
+++ src/main/java/com/practice/OAuth2/domain/user/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.practice.OAuth2.domain.user.repository;
 
 import com.practice.OAuth2.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
@@ -17,5 +20,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // 닉네임 중복 체크 등에 사용
     Boolean existsByNickname(String nickname);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE users SET deleted_at = NULL WHERE email = :email", nativeQuery = true)
+    int restoreUser(String email);
 
 }
