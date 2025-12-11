d1e9cf27d8d473dcc6ccf27a2344848e41c0e733
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2025-12-11 16:29:18 +0900

    feature : attraction 도메인 장소 검색 기능 구현

---
+++ src/main/java/com/practice/OAuth2/domain/attraction/service/AttractionService.java
@@ -0,0 +1,27 @@
+package com.practice.OAuth2.domain.attraction.service;
+
+import com.practice.OAuth2.domain.attraction.dto.AttractionResponse;
+import com.practice.OAuth2.domain.attraction.repository.AttractionRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AttractionService {
+    private final AttractionRepository attractionRepository;
+
+    public List<AttractionResponse> searchAttractions(String keyword) {
+        if (keyword == null || keyword.trim().isEmpty()) {
+            return List.of(); // 검색어 없으면 빈 리스트
+        }
+
+        return attractionRepository.findByTitleContaining(keyword).stream()
+                .map(AttractionResponse::new)
+                .collect(Collectors.toList());
+    }
+}
