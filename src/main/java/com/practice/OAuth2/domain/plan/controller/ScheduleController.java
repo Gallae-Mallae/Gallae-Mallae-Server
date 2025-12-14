767f4dcc173b652353c3c8d8beee91b56b44cc51
sangji <139340193+theundergroundt@users.noreply.github.com>
2025-12-14 16:05:51 +0900

    Feature : plan 도메인 redis lock 추가 및 로직 개선 (#54)
    * feature : redis lock 적용 및 스케줄 동시 편집 구현
    
    * refactor : 스케줄 resize API 요청 방식 간소화 및 미사용 DTO 삭제
    
    * feature : 스케줄 service soft delete메서드 구현
    
    * feature : 스케줄 controller soft delete메서드 구현

--- src/main/java/com/practice/OAuth2/domain/plan/controller/ScheduleController.java
+++ src/main/java/com/practice/OAuth2/domain/plan/controller/ScheduleController.java
@@ -2,7 +2,6 @@ package com.practice.OAuth2.domain.plan.controller;
 
 import com.practice.OAuth2.domain.plan.dto.ScheduleCreateRequest;
 import com.practice.OAuth2.domain.plan.dto.ScheduleMoveRequest;
-import com.practice.OAuth2.domain.plan.dto.ScheduleResizeRequest;
 import com.practice.OAuth2.domain.plan.service.ScheduleService;
 import com.practice.OAuth2.domain.user.entity.User;
 import com.practice.OAuth2.domain.user.repository.UserRepository;
@@ -10,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,17 +35,16 @@ public class ScheduleController {
         return ResponseEntity.ok().build();
     }
 
-    // 스케줄 시간 늘리기
+    // 스케줄 블록 시간 늘리기
     @PatchMapping("/{blockId}/resize")
     public ResponseEntity<Void> resizeBlock(
-            @PathVariable Long blockId,
-            @RequestBody ScheduleResizeRequest request
+            @PathVariable Long blockId
     ) {
-        scheduleService.resizeScheduleBlock(blockId, request.getNewEndTime());
+        scheduleService.resizeScheduleBlock(blockId);
         return ResponseEntity.ok().build();
     }
 
-    // 스케줄 이동
+    // 스케줄 블록 이동
     @PatchMapping("/{blockId}/position")
     public ResponseEntity<Void> moveScheduleBlock(
             @PathVariable Long blockId,
@@ -66,6 +65,22 @@ public class ScheduleController {
                 request.getNewStartTime()
         );
 
+        return ResponseEntity.ok().build();
+    }
+
+    // 스케줄 블록 삭제
+    @DeleteMapping("/{blockId}")
+    public ResponseEntity<Void> deleteBlock(
+            @PathVariable Long blockId,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        String email = userDetails.getUsername();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 이메일입니다."));
+
+        Long userId = user.getUserId();
+
+        scheduleService.deleteScheduleBlock(userId, blockId);
         return ResponseEntity.ok().build();
     }
 }
