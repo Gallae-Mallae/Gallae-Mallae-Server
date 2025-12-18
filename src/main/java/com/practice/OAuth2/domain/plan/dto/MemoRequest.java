fef7ee0da29485f8c1cc9f5374c8b54734cac672
theundergroundt <139340193+theundergroundt@users.noreply.github.com>
2025-12-18 23:05:09 +0900

    feature : 리소스 중심 url 위해서 blockId 받기

--- src/main/java/com/practice/OAuth2/domain/plan/dto/MemoRequest.java
+++ src/main/java/com/practice/OAuth2/domain/plan/dto/MemoRequest.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MemoRequest {
+    private Long blockId;
     private String content;
     private String linkUrl;
     private String type;
