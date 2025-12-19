988859b302f56ad49d15be46dfab158c146cd24b
theundergroundt <sam010842@gmail.com>
2025-12-19 11:25:11 +0900

    feature : 이미지 미리보기 메서드 추가

--- src/main/java/com/practice/OAuth2/domain/scrap/controller/ScrapController.java
+++ src/main/java/com/practice/OAuth2/domain/scrap/controller/ScrapController.java
@@ -1,24 +1,19 @@
 package com.practice.OAuth2.domain.scrap.controller;
 
+import com.practice.OAuth2.domain.scrap.dto.LinkMetadataResponse;
 import com.practice.OAuth2.domain.scrap.dto.ScrapFolderResponse;
 import com.practice.OAuth2.domain.scrap.dto.ScrapReqest;
 import com.practice.OAuth2.domain.scrap.dto.ScrapReqest.CreateScrap;
 import com.practice.OAuth2.domain.scrap.dto.ScrapReqest.CreateScrapFolder;
 import com.practice.OAuth2.domain.scrap.dto.ScrapResponse;
 import com.practice.OAuth2.domain.scrap.service.ScrapService;
+import com.practice.OAuth2.domain.scrap.service.UrlMetadataService;
 import com.practice.OAuth2.global.security.UserPrincipal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -111,5 +106,14 @@ public class ScrapController {
 
         scrapService.deleteScrap(principal.getId(), scrapId);
         return ResponseEntity.ok("스크랩이 삭제되었습니다.");
+    }
+
+    // 미리보기
+    private final UrlMetadataService urlMetadataService;
+
+    @GetMapping("/preview")
+    public ResponseEntity<LinkMetadataResponse> getLinkPreview(@RequestParam String url) {
+        LinkMetadataResponse response = urlMetadataService.extractMetadata(url);
+        return ResponseEntity.ok(response);
     }
 }
