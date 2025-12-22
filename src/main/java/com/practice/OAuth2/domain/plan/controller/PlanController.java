4466e87cdd5e6400332f98aae3784cedddf981a0
theundergroundt <sam010842@gmail.com>
2025-12-22 15:24:25 +0900

    feature : 여행 목록 조회 API 구현

--- src/main/java/com/practice/OAuth2/domain/plan/controller/PlanController.java
+++ src/main/java/com/practice/OAuth2/domain/plan/controller/PlanController.java
@@ -1,9 +1,6 @@
 package com.practice.OAuth2.domain.plan.controller;
 
-import com.practice.OAuth2.domain.plan.dto.PlanCreateRequest;
-import com.practice.OAuth2.domain.plan.dto.PlanJoinRequest;
-import com.practice.OAuth2.domain.plan.dto.PlanResponse;
-import com.practice.OAuth2.domain.plan.dto.PlanUpdateRequest;
+import com.practice.OAuth2.domain.plan.dto.*;
 import com.practice.OAuth2.domain.plan.service.PlanService;
 import com.practice.OAuth2.domain.user.entity.User;
 import com.practice.OAuth2.domain.user.repository.UserRepository;
@@ -12,18 +9,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/plans")
 public class PlanController {
     private final PlanService planService;
+    private final UserRepository userRepository;
 
     // 여행 생성
     @PostMapping()
@@ -34,6 +29,14 @@ public class PlanController {
 
         PlanResponse response = planService.createPlan(request, principal.getId());
         return ResponseEntity.ok(response);
+    }
+
+    // 여행 목록 조회
+    @GetMapping()
+    public ResponseEntity<List<PlanListResponse>> getMyPlans(
+            @AuthenticationPrincipal UserPrincipal principal
+    ) {
+        return ResponseEntity.ok(planService.getMyPlans(principal.getId()));
     }
 
     // 초대 코드로 입장
