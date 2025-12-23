c45fe1d144bccf9d6f052781276d7de110cbcb21
sangji <139340193+theundergroundt@users.noreply.github.com>
2025-12-23 09:43:06 +0900

    Feature : 메모 조회 참여자 조회 수정 (#92)
    * feature : blockId별 메모 목록 조회 API 구현
    
    * modify : 참여자 조회 nickname -> name으로 변경

--- src/main/java/com/practice/OAuth2/domain/plan/controller/MemoController.java
+++ src/main/java/com/practice/OAuth2/domain/plan/controller/MemoController.java
@@ -1,16 +1,11 @@
 package com.practice.OAuth2.domain.plan.controller;
 
 import com.practice.OAuth2.domain.plan.dto.MemoRequest;
+import com.practice.OAuth2.domain.plan.dto.MemoResponse;
 import com.practice.OAuth2.domain.plan.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +24,13 @@ public class MemoController {
         // blockId를 DTO에서 꺼내서 넘김
         memoService.createMemo(blockId, request);
         return ResponseEntity.ok().build();
+    }
+
+    // 메모 목록 조회
+    @GetMapping("/{blockId}")
+    public ResponseEntity<List<MemoResponse>> getMemos(
+            @PathVariable Long blockId) {
+        return ResponseEntity.ok(memoService.getMemos(blockId));
     }
 
     // 메모 수정
