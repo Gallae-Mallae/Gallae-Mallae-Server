f7911fa741b472ca1a4ef5e5ad4ea79fbce5891f
theundergroundt <sam010842@gmail.com>
2025-12-23 09:38:27 +0900

    modify : 참여자 조회 nickname -> name으로 변경

--- src/main/java/com/practice/OAuth2/domain/plan/dto/PlanMemberResponse.java
+++ src/main/java/com/practice/OAuth2/domain/plan/dto/PlanMemberResponse.java
@@ -8,14 +8,14 @@ import lombok.Getter;
 public class PlanMemberResponse {
     private Long memberId;
     private Long userId;
-    private String nickname;
+    private String name;
     private String profileImageUrl;
 
     public PlanMemberResponse(PlanMember planMember) {
         this.memberId = planMember.getMemberId();
         // PlanMember -> User 접근
         this.userId = planMember.getUser().getUserId();
-        this.nickname = planMember.getUser().getNickname(); // User 엔티티 필드명에 맞게 수정
+        this.name = planMember.getUser().getName(); // User 엔티티 필드명에 맞게 수정
         this.profileImageUrl = planMember.getUser().getProfileImageUrl();
     }
 }
